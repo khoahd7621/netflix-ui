398c004d17e66ceb10c33152a2b980ea68a2e262
Hoang Dang Khoa <hoangdangkhoa7621@gmail.com>
2022-06-13 15:48:14 +0700

    Creating the Jumbotron component, styled components GlobalStyles and implementing Normalize.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'normalize.css';
+import App from './App';
+import { GlobalStyles } from './global-style';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>
+);
