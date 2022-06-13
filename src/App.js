51139326f6eb25665dda91c7ab9f10f480e3be14
Hoang Dang Khoa <hoangdangkhoa7621@gmail.com>
2022-06-13 16:04:57 +0700

    Item component, Item component direction prop, container derivative component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import React from "react";
-import jumboData from "./fixtures/jumbo.json";
-import Jumbotron from "./components/Jumbotron";
+import { JumbotronContainer } from "./containers/jumbotron";
 
 function App() {
-  return (
-    <Jumbotron.Container>
-      {jumboData.map((item) => (
-        <Jumbotron key={item.id} direction={item.direction}>
-          <Jumbotron.Pane>
-            <Jumbotron.Title>{item.title}</Jumbotron.Title>
-            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-          </Jumbotron.Pane>
-          <Jumbotron.Pane>
-            <Jumbotron.Image src={item.image} alt={item.alt} />
-          </Jumbotron.Pane>
-        </Jumbotron>
-      ))}
-    </Jumbotron.Container>
-  );
+  return <JumbotronContainer />;
 }
 
 export default App;
