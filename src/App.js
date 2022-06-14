7569e0557a07ac280d5595c3c615710ad34ba743
Hoang Dang Khoa <hoangdangkhoa7621@gmail.com>
2022-06-14 21:28:55 +0700

    Add accordion components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import FooterContainer from "./containers/footer";
+import { FaqsContainer } from "./containers/fqs";
 import { JumbotronContainer } from "./containers/jumbotron";
 
 function App() {
   return (
     <>
       <JumbotronContainer />
+      <FaqsContainer />
       <FooterContainer />
     </>
   );
