7e68b9aa29f27e9197dadbd1a99169482ba1298c
Hoang Dang Khoa <hoangdangkhoa7621@gmail.com>
2022-06-16 22:58:26 +0700

    Add form component and add sign in page

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Accordion } from "./Accordion/Accordion";
 export { default as OptForm } from "./OptForm/OptForm";
 export { default as Header } from "./Header/Header";
 export { default as Feature } from "./Feature/Feature";
+export { default as Form } from "./Form/Form";
--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { HeaderContainer, FooterContainer } from "../containers/index";
+import { Form } from "../components";
 
 const Signin = () => {
-  return <div>Signin</div>;
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  // check form input elements are valid
+  // email & password
+  const isInvalid = password === "" || emailAddress === "";
+  const handleSignIn = (event) => {
+    event.preventDefault();
+
+    // firebase work here!
+  };
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignIn} method="POST">
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              placeholder="Password"
+              autoComplete="off"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+          <Form.Text>
+            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. Learn more.
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 };
 
 export default Signin;
