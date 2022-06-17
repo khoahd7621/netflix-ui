d894a13194b49ee461343db731c4495f2e1badb7
Hoang Dang Khoa <hoangdangkhoa7621@gmail.com>
2022-06-17 19:24:30 +0700

    Add search by fuse.js

--- src/containers/BrowseContainer.js
+++ src/containers/BrowseContainer.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState, useEffect } from "react";
+import Fuse from "fuse.js";
 
 import { FirebaseContext } from "../context/Firebase";
 import SelectProfileContainer from "./SelectProfileContainer";
@@ -26,6 +27,20 @@ const BrowseContainer = ({ slides }) => {
   useEffect(() => {
     setSlideRows(slides[category]);
   }, [slides, category]);
+
+  useEffect(() => {
+    const fuse = new Fuse(slideRows, {
+      keys: ["data.description", "data.title", "data.genre"],
+    });
+
+    const results = fuse.search(searchTerm).map(({ item }) => item);
+
+    if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+      setSlideRows(results);
+    } else {
+      setSlideRows(slides[category]);
+    }
+  }, [searchTerm]);
 
   return profile.displayName ? (
     <>
